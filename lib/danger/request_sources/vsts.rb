b067f066bae2f18968262f0386ffd55e2cba5b0b
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2017-08-29 17:39:21 -0400

    tess

--- lib/danger/request_sources/vsts.rb
+++ lib/danger/request_sources/vsts.rb
@@ -84,7 +84,7 @@ module Danger
           thread_id = c[:id]
           puts thread_id
           comment = c[:comments].first
-          puts comment
+          puts comment[:id]
           puts comment[:content]
           @api.delete_comment(thread_id, comment[:id]) if comment[:content] =~ /generated_by_#{danger_id}/
         end
